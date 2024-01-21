63b6c971d0c1e891d5b73bd2cb5970245761a64f
Horev Vladimir <vshorev@yandex.ru>
2024-01-21 12:40:38 +0300

    Create Понимание логических значений.js

---
+++ Понимание логических значений.js
@@ -0,0 +1,14 @@
+// Другой тип данных — Boolean . Логические значения могут иметь только одно из двух значений: trueили false. По сути, это маленькие переключатели, где trueвключено и falseвыключено. Эти два состояния являются взаимоисключающими.
+
+// Примечание. Логические значения никогда не записываются в кавычках. Строки "true"и "false"не являются логическими значениями и не имеют особого значения в JavaScript.
+
+// Измените welcomeToBooleansфункцию так, чтобы она возвращала trueвместо false.
+
+
+function welcomeToBooleans() {
+    // Only change code below this line
+  
+    return true; // Change this line
+  
+    // Only change code above this line
+  }
