6882590f67f40a09afcb96c2a8b1b939b873dede
Horev Vladimir <vshorev@yandex.ru>
2024-01-22 18:30:41 +0300

    Create Добавьте новые свойства к объекту JavaScript.js

---
+++ Добавьте новые свойства к объекту JavaScript.js
@@ -0,0 +1,31 @@
+// Добавьте новые свойства к объекту JavaScript
+// Вы можете добавлять новые свойства к существующим объектам JavaScript так же, как если бы вы их изменяли.
+
+// Вот как мы добавим barkсвойство в ourDog:
+
+// ourDog.bark = "bow-wow";
+// или
+
+// ourDog["bark"] = "bow-wow";
+// Теперь, когда мы оценим ourDog.bark, мы получим его лай bow-wow.
+
+// Пример:
+
+// const ourDog = {
+//   "name": "Camper",
+//   "legs": 4,
+//   "tails": 1,
+//   "friends": ["everything!"]
+// };
+
+// ourDog.bark = "bow-wow";
+// Добавьте barkсвойство myDogи установите его для звука собаки, например «гав». Вы можете использовать обозначение через точку или скобку.
+
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+  myDog.bark='гав'
